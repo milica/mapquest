484f8ebc8de20dc0b84dbf7bc2884b2f6c8baf40
Damnjan <damnjanj@gmail.com>
2014-12-07 20:30:35 +0100

    Map Document

--- core/module/Application/src/Application/Document/Map.php
+++ core/module/Application/src/Application/Document/Map.php
@@ -9,16 +9,45 @@ class Map {
     /** @ODM\Id */
     private $id;
 
+    /** @ODM\Field(type="string") */
+    private $title;
+
+    /** @ODM\ReferenceOne(targetDocument="User") */
+    private $user;
+
+
     // ******* SETTERS ******* //
+
+    public function setTitle($val) {
+        if(empty($val)){return false;}
+        $this->title = $val;
+        return true;
+    }
+
+    public function setUser($val){
+        if(!is_a($val, 'Application\Document\User')){ return false;}
+        $this->user = $val;
+        return true;
+    }
 
     // #****** SETTERS ******# //
 
 
     // ******* GETTERS ******* //
+
     public function getId()
     {
         return $this->id;
     }
+
+    public function getTitle(){
+        return $this->title;
+    }
+
+    public function getUser(){
+        return $this->user;
+    }
+
     // #****** GETTERS ******# //
 
 }
