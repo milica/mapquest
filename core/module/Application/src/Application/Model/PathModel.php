61f3e78e9aeb3f28bb2e04e0edacdb5c69938885
Damnjan <damnjanj@gmail.com>
2014-12-08 06:55:56 +0100

    area id

--- core/module/Application/src/Application/Model/PathModel.php
+++ core/module/Application/src/Application/Model/PathModel.php
@@ -54,8 +54,9 @@ class PathModel extends ApplicationModel{
     public function update($id, $data)
     {
         $result = $this->standardResult();
+        $user = $this->getUser();
 
-        $path_o = $this->getPathObject($id);
+        $path_o = $this->repository->findOneBy(array('participant.id' => $user->getId(), 'area.id' => $id));
         $path_o->setStatus(true);
         $this->dm->persist($path_o);
         $this->dm->flush();
