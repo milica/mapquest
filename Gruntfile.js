9ab0d11940f4790eb2b2b3b1d82174aea0ed5724
Milica Kadic <milica.kadic.87@gmail.com>
2014-12-08 02:32:28 +0100

    fix build

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,7 +54,8 @@ module.exports = function (grunt) {
                         src: [
                             '.tmp',
                             '<%= yeoman.dist %>/{,*/}*',
-                            '!<%= yeoman.dist %>/.git{,*/}*'
+                            '!<%= yeoman.dist %>/.git{,*/}*',
+                            '!<%= yeoman.dist %>/data/*'
                         ]
                     }
                 ]
